c3a3b5f0c5f872b5d0b8808a6b6a37c825ec8d17
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2024-01-14 16:28:31 -0300

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "rgba(255, 255, 255, 0.0)",
+        darkI: "rgba(255, 255, 255, 0.05)",
+        darkII: "rgba(255, 255, 255, 0.07)",
+        darkIII: "rgba(255, 255, 255, 0.09)",
+      },
+    },
   },
   plugins: [],
 }
