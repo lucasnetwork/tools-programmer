0b459e5c7f77f469ee2e474d30a5b88a0c648cbb
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2024-01-21 15:16:05 -0300

    feat: add initial generate_cnpj function

---
+++ src-tauri/src/generate_cnpj.rs
@@ -0,0 +1,33 @@
+
+use rand::Rng;
+#[tauri::command]
+pub fn generate_cnpj() -> String {
+    let mut rng = rand::thread_rng();
+    let n:f64 = 9.0;
+    let n1 = (rng.gen_range(0.0..1.0) * n).round() as u128;
+    let n2 = (rng.gen_range(0.0..1.0) * n).round()as u128;
+    let n3 = (rng.gen_range(0.0..1.0) * n).round()as u128;
+    let n4 = (rng.gen_range(0.0..1.0) * n).round()as u128;
+    let n5 = (rng.gen_range(0.0..1.0) * n).round()as u128;
+    let n6 = (rng.gen_range(0.0..1.0) * n).round()as u128;
+    let n7 = (rng.gen_range(0.0..1.0) * n).round()as u128;
+    let n8 = (rng.gen_range(0.0..1.0) * n).round()as u128;
+    let n9 = 0;//gera_random(n);
+    let n10 = 0;//gera_random(n);
+    let n11 = 0;//gera_random(n);
+    let n12 = 1;//gera_random(n);
+    let mut d1 = n12*2+n11*3+n10*4+n9*5+n8*6+n7*7+n6*8+n5*9+n4*2+n3*3+n2*4+n1*5;
+    d1 = 11 - d1%11;
+    if (d1>=10) {
+        d1 = 0;
+    }
+    let mut d2: u128 = d1*2+n12*3+n11*4+n10*5+n9*6+n8*7+n7*8+n6*9+n5*2+n4*3+n3*4+n2*5+n1*6;
+    d2 = 11 - d2%11;
+    if d2>=10{
+        d2 = 0;
+    }
+
+
+    format!("{}{}.{}{}{}.{}{}{}/{}{}{}{}-{}{}", n1,n2, n3,n4,n5, n6,n7,n8, n9,n10,n11,n12, d1,d2)
+}
+    
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,12 +2,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "linux")]
 mod generate_cpf;
+mod generate_cnpj;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![generate_cpf::generate_cpf])
+        .invoke_handler(tauri::generate_handler![generate_cpf::generate_cpf,generate_cnpj::generate_cnpj])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
