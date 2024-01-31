2cbe2d6b095d1fd46f9c6ab4d3f53a99c32c4e94
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2024-01-30 22:54:26 -0300

    feat: add validate_cnpj rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,12 +4,12 @@
 mod generate_cpf;
 mod generate_cnpj;
 mod validate_cpf;
-
+mod validate_cnpj;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![generate_cpf::generate_cpf,generate_cnpj::generate_cnpj,validate_cpf::validate_cpf])
+        .invoke_handler(tauri::generate_handler![generate_cpf::generate_cpf,generate_cnpj::generate_cnpj,validate_cpf::validate_cpf,validate_cnpj::validate_cnpj])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/validate_cnpj.rs
@@ -0,0 +1,51 @@
+
+use regex::Regex;
+
+#[tauri::command]
+pub fn validate_cnpj(value_cnpj:String)-> Result<bool,bool>{
+
+    let array_number = [ 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 ];
+    let cnpj_string = value_cnpj.as_str();
+    
+    let re = Regex::new(r"\d+").unwrap();
+    if re.find(cnpj_string).is_none(){
+        return Err(false)
+    }
+    let cpf:Vec<&str> =re.find_iter(cnpj_string).map(|mat| mat.as_str()).collect();
+    let cpf_joined =  cpf.join("");
+    let cpf_only_numbers_string: &str =cpf_joined.as_str();
+    if cpf.is_empty() {
+        return Err(false)
+    }
+    if cpf_only_numbers_string.len() > 14 ||cpf_only_numbers_string.len() <14{
+        return Err(false)
+    }
+
+    let mut n =0;
+    let mut condition = 0;
+
+    for i in 0..12{
+        n +=cpf_only_numbers_string.chars().nth(i).unwrap().to_string().parse::<u32>().unwrap() * array_number[i+1]
+    }
+    n = n%11;
+    if n>= 2{
+        condition =11-n;
+    }
+    if cpf_only_numbers_string.chars().nth(12).unwrap().to_string().parse::<u32>().unwrap() !=condition{
+        return Err(false)
+    }
+    n = 0;
+    for i in 0..=12{
+        n +=cpf_only_numbers_string.chars().nth(i).unwrap().to_string().parse::<u32>().unwrap() * array_number[i]
+    }
+    let mut condition = 0;
+    n = n%11;
+    if (n>= 2){
+        condition =11-n;
+    }
+    if cpf_only_numbers_string.chars().nth(13).unwrap().to_string().parse::<u32>().unwrap() !=condition{
+        return Err(false)
+
+    }
+    Ok(true)
+}
