26e35e75e294eed68bdad66c094ba8e121c881a2
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2024-02-06 21:13:03 -0300

    feat: add texarea component

---
+++ src/components/TextArea/index.tsx
@@ -0,0 +1,15 @@
+import { type JSX } from 'solid-js';
+
+const TextArea = (props: JSX.TextareaHTMLAttributes<HTMLTextAreaElement>) => {
+	return (
+		<textarea
+			class="text-white outline-none p-2 border-primary border px-4 py-2 rounded-sm"
+			style={{
+				background: 'none',
+			}}
+			{...props}
+		/>
+	);
+};
+
+export default TextArea;
