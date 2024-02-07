e049d1906bd5c43e63ee39713f37bcc721f44abe
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2024-02-06 21:13:24 -0300

    feat: add route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import { Router, Route } from '@solidjs/router';
 import GenerateCPF from '../pages/cpf';
 import GenerateCNPJ from '../pages/cnpj/';
 import Init from '../pages/Init';
+import ConvertNumbers from '../pages/ConvertNumbers';
 const Routers = () => (
 	<Router>
 		<Route path="/" component={DefaultLayout}>
 			<Route path="/" component={Init} />
 			<Route path="/cpf" component={GenerateCPF} />
+			<Route path="/convert-numbers" component={ConvertNumbers} />
 			<Route path="/cnpj" component={GenerateCNPJ} />
 		</Route>
 	</Router>
